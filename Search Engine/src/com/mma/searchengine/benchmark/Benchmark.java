2145b64708a776a19509ae531d3b261149769129
Andreas Precht Poulsen <appo@schantz.com>
2015-12-16 14:13:01 +0100

    Redid test framework

--- Search Engine/src/com/mma/searchengine/benchmark/Benchmark.java
+++ Search Engine/src/com/mma/searchengine/benchmark/Benchmark.java
@@ -2,6 +2,7 @@ package com.mma.searchengine.benchmark;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -11,151 +12,133 @@ import com.mma.searchengine.part4.HashMap;
 
 public class Benchmark {
 	
-	private static final int SETUP_ITERATIONS	= 1;
 	private static final int SEARCH_ITERATIONS	= 10000;
 	
-	private static void testPart1(String filename, String searchWord) {
+	private static TestResult testPart1(String filename, String[] searchWords) {
 		try {
-			long[] initTimes = new long[SETUP_ITERATIONS];
-			com.mma.searchengine.part1.HTMLlist htmlList = null;
-			for(int i = 0; i < SETUP_ITERATIONS; i++) {
-				long beforeListInit = System.nanoTime();
-				htmlList = com.mma.searchengine.part1.Searcher.readHtmlList(filename);
-				long listInitTime = System.nanoTime() - beforeListInit;
-				initTimes[i] = listInitTime;
-			}
+			long beforeListInit = System.nanoTime();
+			com.mma.searchengine.part1.HTMLlist htmlList = com.mma.searchengine.part1.Searcher.readHtmlList(filename);
+			long initTime = System.nanoTime() - beforeListInit;
 			
 			long[] searchTimes = new long[SEARCH_ITERATIONS]; 
 			for(int i = 0; i < SEARCH_ITERATIONS; i++) {
 				long beforeSearch = System.nanoTime();
-				com.mma.searchengine.part1.Searcher.exists(htmlList, searchWord);
+				for(String s : searchWords) {
+					com.mma.searchengine.part1.Searcher.exists(htmlList, s);
+				}
 				long searchTime = System.nanoTime() - beforeSearch;
-				searchTimes[i] = searchTime;
+				searchTimes[i] = (searchTime/searchWords.length);
 			}
 			
-			printResults("Part 1", initTimes, searchTimes);
+			return computeTestResults("part1", searchTimes, initTime);
 		} catch(IOException e) {
 			System.out.println("testPart1 failed");
 			e.printStackTrace();
+			return null;
 		}
 	}
 	
-	private static void testPart2(String filename, String searchWord) {
+	private static TestResult testPart2(String filename, String[] searchWords) {
 		try {
-			long[] initTimes = new long[SETUP_ITERATIONS];
-			com.mma.searchengine.part2.HTMLlist htmlList = null;
-			for(int i = 0; i < SETUP_ITERATIONS; i++) {
-				long beforeListInit = System.nanoTime();
-				htmlList = com.mma.searchengine.part2.Searcher.readHtmlList(filename);
-				long listInitTime = System.nanoTime() - beforeListInit;
-				initTimes[i] = listInitTime;
-			}
+			long beforeListInit = System.nanoTime();
+			com.mma.searchengine.part2.HTMLlist htmlList = com.mma.searchengine.part2.Searcher.readHtmlList(filename);
+			long initTime = System.nanoTime() - beforeListInit;
 			
 			long[] searchTimes = new long[SEARCH_ITERATIONS]; 
 			for(int i = 0; i < SEARCH_ITERATIONS; i++) {
 				long beforeSearch = System.nanoTime();
-				com.mma.searchengine.part2.Searcher.exists(htmlList, searchWord);
+				for(String s : searchWords) {
+					com.mma.searchengine.part2.Searcher.exists(htmlList, s);
+				}
 				long searchTime = System.nanoTime() - beforeSearch;
-				searchTimes[i] = searchTime;
+				searchTimes[i] = (searchTime/searchWords.length);
 			}
 			
-			printResults("Part 2", initTimes, searchTimes);
+			return computeTestResults("part2", searchTimes, initTime);
 		} catch(IOException e) {
-			System.out.println("testPart1 failed");
+			System.out.println("testPart2 failed");
 			e.printStackTrace();
+			return null;
 		}
 	}
 	
-	private static void testPart3(String filename, String searchWord) {
+	private static TestResult testPart3(String filename, String[] searchWords) {
 		try {
-			long[] initTimes = new long[SETUP_ITERATIONS];
-			com.mma.searchengine.part3.WordList wordList = null;
-			for(int i = 0; i < SETUP_ITERATIONS; i++) {
-				long beforeListInit = System.nanoTime();
-				wordList = com.mma.searchengine.part3.WordList.construct(new BufferedReader(new InputStreamReader(new FileInputStream(filename), StandardCharsets.UTF_8)));
-				long listInitTime = System.nanoTime() - beforeListInit;
-				initTimes[i] = listInitTime;
-			}
+			long beforeListInit = System.nanoTime();
+			com.mma.searchengine.part3.WordList wordList = com.mma.searchengine.part3.WordList.construct(new BufferedReader(new InputStreamReader(new FileInputStream(filename), StandardCharsets.UTF_8)));
+			long initTime = System.nanoTime() - beforeListInit;
 			
 			long[] searchTimes = new long[SEARCH_ITERATIONS]; 
 			for(int i = 0; i < SEARCH_ITERATIONS; i++) {
 				long beforeSearch = System.nanoTime();
-				com.mma.searchengine.part3.SearchCmd.exists(wordList, searchWord);
+				for(String s : searchWords) {
+					com.mma.searchengine.part3.SearchCmd.exists(wordList, s);
+				}
 				long searchTime = System.nanoTime() - beforeSearch;
-				searchTimes[i] = searchTime;
+				searchTimes[i] = (searchTime/searchWords.length);
 			}
 			
-			printResults("Part 3", initTimes, searchTimes);
+			return computeTestResults("part3", searchTimes, initTime);
 		} catch(IOException e) {
-			System.out.println("testPart1 failed");
+			System.out.println("testPart3 failed");
 			e.printStackTrace();
+			return null;
 		}
 	}
 	
-	private static void testPart4(String filename, String searchWord) {
+	private static TestResult testPart4(String filename, String[] searchWords) {
 		try {
-			long[] initTimes = new long[SETUP_ITERATIONS];
-			HashMap<String, URLList> wordList = null;
-			for(int i = 0; i < SETUP_ITERATIONS; i++) {
-				long beforeListInit = System.nanoTime();
-				wordList = com.mma.searchengine.part4.SearchCmd.constructHashMap(new BufferedReader(new InputStreamReader(new FileInputStream(filename), StandardCharsets.UTF_8)));
-				long listInitTime = System.nanoTime() - beforeListInit;
-				initTimes[i] = listInitTime;
-			}
+			long beforeListInit = System.nanoTime();
+			HashMap<String, URLList> wordList = com.mma.searchengine.part4.SearchCmd.constructHashMap(new BufferedReader(new InputStreamReader(new FileInputStream(filename), StandardCharsets.UTF_8)));
+			long initTime = System.nanoTime() - beforeListInit;
 			
 			long[] searchTimes = new long[SEARCH_ITERATIONS]; 
 			for(int i = 0; i < SEARCH_ITERATIONS; i++) {
 				long beforeSearch = System.nanoTime();
-				com.mma.searchengine.part4.SearchCmd.exists(wordList, searchWord);
+				for(String s : searchWords) {
+					com.mma.searchengine.part4.SearchCmd.exists(wordList, s);
+				}
 				long searchTime = System.nanoTime() - beforeSearch;
-				searchTimes[i] = searchTime;
+				searchTimes[i] = (searchTime/searchWords.length);
 			}
 			
-			printResults("Part 4", initTimes, searchTimes);
+			return computeTestResults("part4", searchTimes, initTime);
 		} catch(IOException e) {
-			System.out.println("testPart1 failed");
+			System.out.println("testPart4 failed");
 			e.printStackTrace();
+			return null;
 		}
 	}
 	
-	private static void testPart5(String filename, String searchWord) {
+	private static TestResult testPart5(String filename, String[] searchWords) {
 		try {
-			long[] initTimes = new long[SETUP_ITERATIONS];
-			for(int i = 0; i < SETUP_ITERATIONS; i++) {
-				long beforeListInit = System.nanoTime();
-				com.mma.searchengine.part5.Searcher.readHtmlList(filename);
-				long listInitTime = System.nanoTime() - beforeListInit;
-				initTimes[i] = listInitTime;
-			}
-			
-			long[] searchTimes = new long[SEARCH_ITERATIONS]; 
+			long beforeListInit = System.nanoTime();
+			com.mma.searchengine.part5.Searcher.readHtmlList(filename);
+			long initTime = System.nanoTime() - beforeListInit;
+
+				long[] searchTimes = new long[SEARCH_ITERATIONS]; 
 			for(int i = 0; i < SEARCH_ITERATIONS; i++) {
 				long beforeSearch = System.nanoTime();
-				com.mma.searchengine.part5.Searcher.exists(searchWord);
+				for(String s : searchWords) {
+					com.mma.searchengine.part5.Searcher.exists(s);
+				}
 				long searchTime = System.nanoTime() - beforeSearch;
-				searchTimes[i] = searchTime;
+				searchTimes[i] = (searchTime/searchWords.length);
 			}
 			
-			printResults("Part 5", initTimes, searchTimes);
+			return computeTestResults("part5", searchTimes, initTime);
 		} catch(IOException e) {
-			System.out.println("testPart1 failed");
+			System.out.println("testPart5 failed");
 			e.printStackTrace();
+			return null;
 		}
 	}
 	
-	public static void printResults(String name, long[] listInitTime, long[] searchTime) {
-		System.out.println(name);
-		printResult("Data structure init time", listInitTime);
-		printResult("Search time", searchTime);
-		
-		System.out.println();
-	}
-	
-	private static void printResult(String heading, long[] arr) {
+	private static TestResult computeTestResults(String name, long[] searchTimes, long initTime) {
 		long totalTime = 0;
 		long maxTime = Long.MIN_VALUE;
 		long minTime = Long.MAX_VALUE;
-		for(long l : arr) {
+		for(long l : searchTimes) {
 			totalTime += l;
 			if(l > maxTime) {
 				maxTime = l;
@@ -164,48 +147,155 @@ public class Benchmark {
 				minTime = l;
 			}
 		}
-		final double mean = totalTime / arr.length;
+		final double mean = totalTime / searchTimes.length;
 		
 		double variance = 0;
-		for(int i = 0; i < arr.length; i++) {
-			variance += Math.pow((arr[i] - mean), 2d);
+		for(int i = 0; i < searchTimes.length; i++) {
+			variance += Math.pow((searchTimes[i] - mean), 2d);
 		}
-		variance /= arr.length;
+		variance /= searchTimes.length;
 		
+		variance = nsToMs(variance);
 		final double standardDeviation = Math.sqrt(variance);
-		
-		System.out.println(heading);
-		System.out.println("Times tested: " + arr.length);
-		System.out.println(format("Mean:          ", mean));
-		System.out.println(format("Max:           ", maxTime));
-		System.out.println(format("Min:           ", minTime));
-		System.out.println(format("Variance:      ", variance));
-		System.out.println(format("Std deviation: ", standardDeviation));
+		return new TestResult(name, nsToMs(initTime), nsToMs(mean), nsToMs(maxTime), nsToMs(minTime), variance, standardDeviation);
 	}
 	
-	/**
-	 * 
-	 * @param text
-	 * @param d Double in ns
-	 * @return return in ms
-	 */
-	private static String format(String text, double d) {
-		return String.format(text + "%12.5f ms", d/1_000_000);
+	private static void printResults(TestResult... testResults) {
+		for(TestResult tr : testResults) {
+			printResult(tr);
+		}
 	}
 	
-	private static String format(String text, long l) {
-		return format(text , (double) l);
+	private static void printResult(TestResult testResult) {
+		System.out.println(testResult.getName());
+		System.out.println("Times tested: " + SEARCH_ITERATIONS);
+		System.out.println(format("Init time:     ", testResult.getInitTime()));
+		System.out.println(format("Mean:          ", testResult.getMean()));
+		System.out.println(format("Max:           ", testResult.getMax()));
+		System.out.println(format("Min:           ", testResult.getMin()));
+		System.out.println(format("Variance:      ", testResult.getVariance()));
+		System.out.println(format("Std deviation: ", testResult.getStdDeviation()));
 	}
+	
+	private static void writeResultsToFile(TestResult... testResults) {
+		StringBuilder sb = new StringBuilder();
 
+		sb.append("Data Type\t");
+		for(TestResult tr : testResults) {
+			sb.append(tr.getName()).append("\t");
+		}
+		sb.append("\n").append("mean").append("\t");
+		for(TestResult tr : testResults) {
+			sb.append(tr.getMean()).append("\t");
+		}
+		sb.append("\n").append("max").append("\t");
+		for(TestResult tr : testResults) {
+			sb.append(tr.getMax()).append("\t");
+		}
+		sb.append("\n").append("min").append("\t");
+		for(TestResult tr : testResults) {
+			sb.append(tr.getMin()).append("\t");
+		}
+		try {
+			new FileWriter("mean-max-min.dat").append(sb.toString()).close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		sb = new StringBuilder();
+		sb.append("Data Type\t").append("initTime\n");
+		for(TestResult tr : testResults) {
+			sb.append(tr.getName()).append("\t").append(tr.getInitTime()).append("\n");
+		}
+		try {
+			new FileWriter("init-time.dat").append(sb.toString()).close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		sb = new StringBuilder();
+		sb.append("Data Type\t").append("stdDeviation\n");
+		for(TestResult tr : testResults) {
+			sb.append(tr.getName()).append("\t").append(tr.getStdDeviation()).append("\n");
+		}
+		try {
+			new FileWriter("std-deviation.dat").append(sb.toString()).close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static double nsToMs(double d) {
+		return d / 1_000_000;
+	}
+	
+	private static double nsToMs(long l) {
+		return ((double) l) / 1_000_000;
+	}
+	
+	private static String format(String text, double d) {
+		return String.format(text + "%12.6f ms", d);
+	}
+	
 	public static void main(String[] args) {
 		final String filename = "data/itcwww-big.txt";
-		final String searchWord = "på";
-		testPart1(filename, searchWord);
-		testPart2(filename, searchWord);
-		testPart3(filename, searchWord);
-		testPart4(filename, searchWord);
-		testPart5(filename, searchWord);
+		final String[] searchWords = new String[] {"på", "forudsætninger", "varchar"};
+		TestResult testPart1 = testPart1(filename, searchWords);
+		TestResult testPart2 = testPart2(filename, searchWords);
+		TestResult testPart3 = testPart3(filename, searchWords);
+		TestResult testPart4 = testPart4(filename, searchWords);
+		TestResult testPart5 = testPart5(filename, searchWords);
+		
+		printResults(testPart1, testPart2, testPart3, testPart4, testPart5);
+		writeResultsToFile(testPart1, testPart2, testPart3, testPart4, testPart5);
 	}
+}
+
+class TestResult {
 	
+	private final String name;
+	private final double initTime;
+	private final double mean;
+	private final double max;
+	private final double min;
+	private final double variance;
+	private final double stdDeviation;
 	
+	public TestResult(String name, double initTime, double mean, double max, double min, double variance, double stdDeviation) {
+		this.name = name;
+		this.initTime = initTime;
+		this.mean = mean;
+		this.max = max;
+		this.min = min;
+		this.variance = variance;
+		this.stdDeviation = stdDeviation;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getInitTime() {
+		return initTime;
+	}
+
+	public double getMean() {
+		return mean;
+	}
+
+	public double getMax() {
+		return max;
+	}
+
+	public double getMin() {
+		return min;
+	}
+
+	public double getVariance() {
+		return variance;
+	}
+
+	public double getStdDeviation() {
+		return stdDeviation;
+	}
 }
